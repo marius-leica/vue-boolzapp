87bdc28751fd57ca9ad758608a119e49f54605eb
marius-leica <mariusleica@gmail.com>
2022-06-15 15:07:01 +0200

    ticket

--- milestone02/main.js
+++ milestone02/main.js
@@ -3,6 +3,8 @@ const contatti = [
     name: "Michele",
     avatar: "_1",
     visible: true,
+    isActive: false,
+
     messages: [
       {
         date: "10/01/2020 15:30:55",
@@ -25,6 +27,8 @@ const contatti = [
     name: "Fabio",
     avatar: "_2",
     visible: true,
+    isActive: false,
+
     messages: [
       {
         date: "20/03/2020 16:30:00",
@@ -167,6 +171,14 @@ const contatti = [
 
 Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
+
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 */
 
 const app = new Vue({
@@ -174,6 +186,7 @@ const app = new Vue({
   data: {
     contacts: contatti,
     currentContact: contatti[0],
+    currentContactMessages: contatti[0].messages,
   },
   methods: {
     selectContact(contact) {
